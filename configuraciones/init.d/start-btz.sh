dd6f4cdb11e4674953aa6a5f18cc2ad41369617e
root <root@raspberrypi.(none)>
2016-02-26 01:32:53 +0100

    Actualizados demonios

--- configuraciones/init.d/start-btz.sh
+++ configuraciones/init.d/start-btz.sh
@@ -1,13 +1,37 @@
+### BEGIN INIT INFO
+# Provides:          start-btz.sh
+# Required-Start:    $all
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start paring bluetooh
+# Description:       Enable bluetooth pair.
+### END INIT INFO
+
 #!/bin/bash
 #Resetea y conecta la conexion bluetooth
 
 MAC_OBD="00:12:6F:10:53:34"
 MAC_MLAIS="0C:00:90:E7:8F:47"
 
-#/etc/init.d/bluetooth restart
-
-/usr/bin/bluez-test-device trusted $MAC_OBD yes
-
-/usr/bin/rfcomm release 0 $MAC_OBD
-
-/usr/bin/rfcomm connect 0 $MAC_OBD
+	case "$1" in
+	start)
+		echo "Iniciando servicio start-rpi..."
+		/usr/bin/bluez-test-device trusted $MAC_OBD yes
+		/usr/bin/rfcomm release 0 $MAC_OBD
+		/usr/bin/rfcomm connect 0 $MAC_OBD
+	;;
+	stop)
+		echo "Deteniendo el servicio start-rpi..."
+		/etc/init.d/bluetooth stop
+	;;
+	restart)
+		echo "Reiniciando el servicio start-btz..."
+		/etc/init.d/bluetooth restart
+	;;
+	*)
+	echo "Modo de empleo: /etc/init.d/start-rpi.sh {start|stop}"
+	exit 1
+	;;
+	esac
+	exit 0
--- configuraciones/init.d/start-rpi.sh
+++ configuraciones/init.d/start-rpi.sh
@@ -1,33 +1,52 @@
-#!/bin/sh
+### BEGIN INIT INFO
+# Provides:          start-rpi.sh
+# Required-Start:    $all
+# Required-Stop:
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start services RPI
+# Description:       Starting all applications
+### END INIT INFO
+
+#!/bin/bash
 #SCRIPT para encender todos los programas necesarios al inicio
 
-#echo -e "\nReseteando Modulo....."
-#programa ModuleOff
-
-echo "0" > /home/ArduPi/blueberry/port.txt
-
-#echo -e "\nEncendiendo Modulo....."
-/home/ArduPi/blueberry/ModuleIgnition
-
-sleep 4
-
-#echo -e "\nIntroducciendo el numero PIN"
-/home/ArduPi/blueberry/SIMPin
-
-sleep 10
-
-/home/ArduPi/blueberry/GPRS/setTime &
-
-sleep 15
-
-/home/ArduPi/blueberry/projectOBDII/applicationPID &
-
-sleep 10
-
-/home/ArduPi/blueberry/geolocation &
-
-
-sleep 120
-
-
-/home/ArduPi/blueberry/GPRS/sendInfoHTTP &
+	case "$1" in
+	start)
+		echo "Iniciando servicio start-rpi..."
+		#echo "Reseteando Modulo....."
+		#/home/ArduPi/blueberry/ModuleOff &
+		echo "0" > /home/ArduPi/blueberry/port.txt
+		sleep 4
+		echo "Encendiendo Modulo....."
+		/home/ArduPi/blueberry/ModuleIgnition &
+		sleep 8
+		echo  "Introducciendo el numero PIN"
+		/home/ArduPi/blueberry/SIMPin &
+		sleep 20
+                /home/ArduPi/blueberry/geolocation &
+		#sleep 10
+		#/home/ArduPi/blueberry/GPRS/setTime &
+		sleep 15
+		/home/ArduPi/blueberry/projectOBDII/applicationPID &
+		sleep 30
+		/home/ArduPi/blueberry/GPRS/sendInfoHTTP &
+	;;
+	stop)
+		echo "Deteniendo el servicio start-rpi..."
+		pkill ModuleOff
+		pkill ModuleIgnition
+		pkill SIMPin
+		pkill setTime
+		pkill applicationPID
+		pkill geolocation
+		pkill sendInfoHTTP
+                echo "0" > /home/ArduPi/blueberry/port.txt
+		/home/ArduPi/blueberry/ModuleOff
+	;;
+	*)
+	echo "Modo de empleo: /etc/init.d/start-rpi.sh {start|stop}"
+	exit 1
+	;;
+	esac
+	exit 0
